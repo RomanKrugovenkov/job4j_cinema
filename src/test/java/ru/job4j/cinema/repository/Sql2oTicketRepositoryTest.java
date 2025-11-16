1b8b30e3f5c704a240df60119bfe2d9da2f569fd
RomanKrugovenkov <krugovenkov@mail.ru>
2025-11-16 18:29:39 +0300

    2. Сервис - Кинотеатр [#504869]

--- src/test/java/ru/job4j/cinema/repository/Sql2oTicketRepositoryTest.java
+++ src/test/java/ru/job4j/cinema/repository/Sql2oTicketRepositoryTest.java
@@ -5,10 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.sql2o.Sql2o;
 import ru.job4j.cinema.configuration.DatasourceConfiguration;
-import ru.job4j.cinema.model.Film;
-import ru.job4j.cinema.model.FilmSession;
-import ru.job4j.cinema.model.Hall;
-import ru.job4j.cinema.model.Ticket;
+import ru.job4j.cinema.model.*;
 
 import java.time.LocalDateTime;
 import java.time.Month;
@@ -22,6 +19,8 @@ class Sql2oTicketRepositoryTest {
     private static Sql2oFilmSessionRepository sql2oFilmSessionRepository;
     private static Sql2oFilmRepository sql2oFilmRepository;
     private static Sql2oHallRepository sql2oHallRepository;
+    private static Sql2oGenreRepository sql2oGenreRepository;
+    private static Sql2oFileRepository sql2oFileRepository;
 
     @BeforeAll
     public static void initRepositories() throws Exception {
@@ -40,6 +39,8 @@ class Sql2oTicketRepositoryTest {
         sql2oFilmSessionRepository = new Sql2oFilmSessionRepository(sql2o);
         sql2oFilmRepository = new Sql2oFilmRepository(sql2o);
         sql2oHallRepository = new Sql2oHallRepository(sql2o);
+        sql2oGenreRepository = new Sql2oGenreRepository(sql2o);
+        sql2oFileRepository = new Sql2oFileRepository(sql2o);
     }
 
     @AfterEach
@@ -48,13 +49,19 @@ class Sql2oTicketRepositoryTest {
         sql2o.open().createQuery("DELETE FROM film_sessions").executeUpdate();
         sql2o.open().createQuery("DELETE FROM films").executeUpdate();
         sql2o.open().createQuery("DELETE FROM halls").executeUpdate();
+        sql2o.open().createQuery("DELETE FROM genres").executeUpdate();
+        sql2o.open().createQuery("DELETE FROM files").executeUpdate();
+
     }
 
     @Test
     public void whenSaveThenGetSame() {
         LocalDateTime startTime = LocalDateTime.of(2025, Month.OCTOBER, 10, 18, 0, 0);
         LocalDateTime endTime = LocalDateTime.of(2025, Month.OCTOBER, 10, 20, 0, 0);
-        var film = sql2oFilmRepository.save(new Film("Matrix", "Нео против системы", 1998, 1, 18, 100, 1));
+
+        var genre = sql2oGenreRepository.save(new Genre("Action"));
+        var file = sql2oFileRepository.save(new File("Matrix", "/images/1.jpg"));
+        var film = sql2oFilmRepository.save(new Film("Matrix", "Нео против системы", 1998, genre.getId(), 18, 100, file.getId()));
         var hall = sql2oHallRepository.save(new Hall("Hall 1", 10, 16, "Малый зал"));
         var session = sql2oFilmSessionRepository.save(new FilmSession(film.getId(), hall.getId(), startTime, endTime, 10));
         var ticket = sql2oTicketRepository.save(new Ticket(session.getId(), 2, 3, 1));
